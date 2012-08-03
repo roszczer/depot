d88c0b5e6f68f99b2d0aa6e53e135c990d33ff5f
Robert Szczerbak <roszczer@gmail.com>
2012-08-03 14:37:09 +0200

    initial commit

---
+++ config/deploy.rb
@@ -0,0 +1,54 @@
+set :user, 'rs'
+set :domain, 'localhost'
+set :application, "depot"
+set :repository,  "#{user}@#{domain}:git/#{application}.git"
+set :deploy_to, "/home/#{user}/#{domain}"
+
+set :scm, 'git'
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, domain               # Your HTTP server, Apache/etc
+role :app, domain   # This may be the same as your `Web` server
+# This is where Rails migrations will run
+role :db, domain, :primary => true
+
+set :deploy_via, :remote_cache
+set :branch, 'master'
+set :scm_verbose, true
+set :use_sudo, false
+set :rails_env, :production
+
+default_run_options[:pty] = true
+
+namespace :deploy do
+  desc "cause Passenger to initiate a restart"
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt" 
+  end
+
+  desc "reload the database with seed data"
+  task :seed do
+    run "cd #{current_path}; rake db:seed RAILS_ENV=#{rails_env}"
+  end
+end
+
+after "deploy:update_code", :bundle_install
+desc "install the necessary prerequisites"
+task :bundle_install, :roles => :app do
+  run "cd #{release_path} && bundle install"
+end
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
